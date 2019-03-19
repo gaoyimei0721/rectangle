4120454da69d7824f768877b1fe22e9cccaf4658
gaoyimei0721 <1272379222@qq.com>
2019-03-19 13:37:21 +0800

    rectangle v0.3 unit test

--- rectangle.js
+++ rectangle.js
@@ -6,13 +6,12 @@ $(function() {
       $area = $('#area');
 
   $btnCal.click(function(){      
-    var w = Number($width.val()),
-        h = Number($height.val());
+    var w = $width.val(),
+        h = $height.val();
 
-    var p = 2 * (w + h),
-        a = w * h;
+    var r = new Rectangle(w, h);
 
-    $perimeter.val(p);
-    $area.val(a);          
+    $perimeter.val(r.perimeter());
+    $area.val(r.area());
   });
 });
