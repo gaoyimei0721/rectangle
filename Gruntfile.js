77f02cad8724c900a48a4780fa2d13959377561a
gaoyimei0721 <1272379222@qq.com>
2019-03-19 14:00:39 +0800

    rectangle v0.4 minify code

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,7 +26,28 @@ module.exports = function (grunt) {
       options: {             
         run: true,                      
         reporter: 'Spec'                                  
-      }  
+      }
+    },
+    htmlmin: {
+      options: {                
+        collapseWhitespace: true,                        
+        preserveLineBreaks: false                                      
+      },
+      files: {                
+        src: './index.html',                        
+        dest: 'dist/index.html'                                
+      }          
+    },
+    cssmin: {            
+      'dist/rectangle.css': 'rectangle.css'                  
+    },
+    uglify: {
+      release:{
+        files: {                    
+          'dist/rectangle.js': 'rectangle.js',                             
+          'dist/calc.js': 'calc.js'                                        
+        }                     
+      }
     }
   });
     
@@ -34,7 +55,11 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-htmlhint');
   grunt.loadNpmTasks('grunt-eslint');
   grunt.loadNpmTasks('grunt-mocha');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   grunt.registerTask('lint', ['htmlhint', 'csslint', 'eslint']);
   grunt.registerTask('unitTest', ['mocha']);
+  grunt.registerTask('minify', ['htmlmin', 'cssmin', 'uglify']);
 };
