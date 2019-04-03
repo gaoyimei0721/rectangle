cd6032699aceb5063a6c07f0adcfd2d66a32f03a
gaoyimei0721 <1272379222@qq.com>
2019-04-03 13:49:45 +0800

    集中校验

--- rectangle.js
+++ rectangle.js
@@ -4,32 +4,9 @@ $(function() {
       $height = $('#height'),
       $btnCal = $('#calculate'),
       $perimeter = $('#perimeter'),
-      $widthValidation = $('#width-validate'),
-      $heightValidation = $('#height-validate'),
       $area = $('#area');
 
-  $width.focusout(function(){
-    var result = valid($width.val());
-
-    if(!result.isOK){
-      $widthValidation.html('宽度' + result.reason);
-      $width.select();
-    } else{
-      $widthValidation.html('');
-    }
-  });
-
-  $height.focusout(function(){
-    var result = valid($height.val());
-
-    if(!result.isOK){
-      $heightValidation.html('高度' + result.reason);
-      $height.select();
-    }else{
-      $heightValidation.html('');
-    }
-  });
-
+ 
   $btnCal.click(function(){      
     var w = $width.val(),
         h = $height.val();
